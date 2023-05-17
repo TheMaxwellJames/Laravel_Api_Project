99f974b3609e11fab86af09aba8b18c1f7205ae1
TheMaxwellJames <maxichanj@gmail.com>
2023-05-17 06:24:14 -0700

    login Api with sanctum

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,25 +5,98 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Hash;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
 
-        $request->validate([
+        // $request->validate([
+        //     'name'=> 'required|min:2|max:100',
+        //     'email'=> 'required|email|unique:users',
+        //     'password'=> 'required|min:6|max:100',
+        //     'confirm_password'=> 'required|same:password',
+        // ]);
+
+        $validator = Validator::make($request->all(), [
             'name'=> 'required|min:2|max:100',
             'email'=> 'required|email|unique:users',
             'password'=> 'required|min:6|max:100',
-            'confirm password'=> 'required|same:password',
+            'confirm_password'=> 'required|same:password',
         ]);
 
+        if ($validator->fails()) {
+            return response()->json([
+                'message'=> 'Validation Fails',
+                'errors'=>$validator->errors()
+            ],422);
+        }
+ 
+      $user=User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password)
+      ]);
 
 
         return response()->json([
-            'message'=> 'Registration'
+            'message'=> 'Registration Successful',
+            'data'=>$user
+        ],200);
+
+    }
+
+
+
+
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+
+            'email'=> 'required|email|',
+            'password'=> 'required',
+          
         ]);
 
-     
+        if ($validator->fails()) {
+            return response()->json([
+                'message'=> 'Validation Fails',
+                'errors'=>$validator->errors()
+            ],422);
+        }
+
+        $user=User::where('email', $request->email)->first();
+
+        if($user)
+        {
+            if(Hash::check($request->password, $user->password))
+            {
+
+                $token=$user->createToken('auth-token')->plainTextToken;
+
+                return response()->json([
+                    'message'=> 'Login Successful',
+                    'token' =>$token,
+                    'data'=>$user
+                   
+                ],200); 
+
+
+            }else{
+                return response()->json([
+                    'message'=> 'Incorrect Credentials',
+                   
+                ],400); 
+            }
+
+        }else{
+            return response()->json([
+                'message'=> 'Incorrect Credentials',
+                'errors'=>$validator->errors()
+            ],400); 
+        }
     }
 }
